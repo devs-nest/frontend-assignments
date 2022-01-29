802ed49fba7ccecebf447958734847a19261ee4e
Bivas-Biswas <bivasbiswas1999@gmail.com>
2022-01-29 14:51:40 +0530

    ♻️ remove all data

--- README.md
+++
@@ -1,2 +0,0 @@
-### Day01
-- Used div, span, p, b, h2, h3, h4, ul tags
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import '../styles/others.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
